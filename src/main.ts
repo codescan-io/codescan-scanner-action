75246b7bfdc51fde193640ee6425d5e682a9a127
Barys Yakavita <barys.yakavita@autorabit.com>
2023-03-06 23:37:40 +0300

    Code cleanup

--- src/main.ts
+++ src/main.ts
@@ -107,7 +107,7 @@ async function run(): Promise<void> {
           )
           .then(data => {
             const json = JSON.parse(data);
-            core.debug('Quality Gate status: ' + json.projectStatus.status)
+            core.debug(`Quality Gate status: ${json.projectStatus.status}`)
             if (json.errors || json.projectStatus.status !== 'OK') {
               core.setFailed("Failed Quality Gate")
             }
